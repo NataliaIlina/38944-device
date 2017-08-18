5fb60343aa8cd15fee21f0871e297f10974579cd
NataliaIlina <metlinova86@gmail.com>
2017-08-18 07:33:26 +0300

    modal map + js

---
+++ js/modal.js
@@ -0,0 +1,24 @@
+var feedback = document.querySelector(".feedback");
+var feedbackOpen = document.querySelector(".button--modal");
+var feedbackClose = document.querySelector(".modal-close--feedback");
+var map = document.querySelector(".modal-map");
+var mapOpen = document.querySelector(".info-block__map");
+var mapClose = document.querySelector(".modal-close--map");
+
+feedbackOpen.addEventListener("click", function (event) {
+  event.preventDefault();
+  feedback.classList.add("feedback--show");
+});
+
+feedbackClose.addEventListener("click", function () {
+  feedback.classList.remove("feedback--show");
+});
+
+mapOpen.addEventListener("click", function (event) {
+  event.preventDefault();
+  map.classList.add("modal-map--show");
+});
+
+mapClose.addEventListener("click", function () {
+  map.classList.remove("modal-map--show");
+});
