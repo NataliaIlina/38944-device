136f5f9a9af2fce96d6e8e6cc80d699a9e29d24b
NataliaIlina <metlinova86@gmail.com>
2017-08-23 11:26:57 +0300

    добавила js

--- js/modal.js
+++ js/modal.js
@@ -5,20 +5,37 @@ var map = document.querySelector(".modal-map");
 var mapOpen = document.querySelector(".info-block__map");
 var mapClose = document.querySelector(".modal-close--map");
 
-feedbackOpen.addEventListener("click", function (event) {
-  event.preventDefault();
-  feedback.classList.add("feedback--show");
-});
 
-feedbackClose.addEventListener("click", function () {
-  feedback.classList.remove("feedback--show");
-});
+// feedbackOpen.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   feedback.classList.add("feedback--show");
+// });
+//
+// feedbackClose.addEventListener("click", function () {
+//   feedback.classList.remove("feedback--show");
+// });
+//
+// mapOpen.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   map.classList.add("modal-map--show");
+// });
+//
+// mapClose.addEventListener("click", function () {
+//   map.classList.remove("modal-map--show");
+// });
 
-mapOpen.addEventListener("click", function (event) {
-  event.preventDefault();
-  map.classList.add("modal-map--show");
-});
 
-mapClose.addEventListener("click", function () {
-  map.classList.remove("modal-map--show");
-});
+
+function modalOpen(button, modal, buttonClose) {
+  button.addEventListener("click", function (event) {
+    event.preventDefault();
+    modal.style.display = "block";
+  });
+  buttonClose.addEventListener("click", function (event) {
+    event.preventDefault();
+    modal.style.display = "none";
+  });
+};
+
+modalOpen(feedbackOpen, feedback, feedbackClose);
+modalOpen(mapOpen, map, mapClose);
