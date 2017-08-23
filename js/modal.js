416b22437ae49c4822511947a0ab787e6ad64107
NataliaIlina <metlinova86@gmail.com>
2017-08-23 20:33:42 +0300

    Правки
    JS + исправление сетки

--- js/modal.js
+++ js/modal.js
@@ -6,27 +6,8 @@ var mapOpen = document.querySelector(".info-block__map");
 var mapClose = document.querySelector(".modal-close--map");
 
 
-// feedbackOpen.addEventListener("click", function (event) {
-//   event.preventDefault();
-//   feedback.classList.add("feedback--show");
-// });
-//
-// feedbackClose.addEventListener("click", function () {
-//   feedback.classList.remove("feedback--show");
-// });
-//
-// mapOpen.addEventListener("click", function (event) {
-//   event.preventDefault();
-//   map.classList.add("modal-map--show");
-// });
-//
-// mapClose.addEventListener("click", function () {
-//   map.classList.remove("modal-map--show");
-// });
-
-
-
 function modalOpen(button, modal, buttonClose) {
+ if(modal){
   button.addEventListener("click", function (event) {
     event.preventDefault();
     modal.style.display = "block";
@@ -35,6 +16,7 @@ function modalOpen(button, modal, buttonClose) {
     event.preventDefault();
     modal.style.display = "none";
   });
+  }
 };
 
 modalOpen(feedbackOpen, feedback, feedbackClose);
