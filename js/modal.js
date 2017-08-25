807fd93b4f06720c43bf197be3c4cb5ad8b3c31e
NataliaIlina <metlinova86@gmail.com>
2017-08-25 17:38:06 +0300

    +анимация на js

--- js/modal.js
+++ js/modal.js
@@ -4,6 +4,9 @@ var feedbackClose = document.querySelector(".modal-close--feedback");
 var map = document.querySelector(".modal-map");
 var mapOpen = document.querySelector(".info-block__map");
 var mapClose = document.querySelector(".modal-close--map");
+var form = feedback.querySelector(".feedback__form");
+var name = form.querySelector("[name=name]");
+var email = form.querySelector("[name=email]");
 
 
 function modalOpen(button, modal, buttonClose) {
@@ -14,10 +17,27 @@ function modalOpen(button, modal, buttonClose) {
   });
   buttonClose.addEventListener("click", function (event) {
     event.preventDefault();
+    if(modal.classList.contains("modal-error")){
+      modal.classList.remove("modal-error");
+    }
     modal.style.display = "none";
   });
   }
 };
+
+form.addEventListener("submit", function(event) {
+  if(!name.value || !email.value){
+    event.preventDefault();
+    if(feedback.classList.contains("modal-error")){
+      feedback.classList.remove("modal-error");
+    }
+    feedback.offsetHeight;
+    feedback.classList.add("modal-error");
+  }
+  else {
+    localStorage.setItem('name', name.value);
+  }
+});
 
 modalOpen(feedbackOpen, feedback, feedbackClose);
 modalOpen(mapOpen, map, mapClose);
